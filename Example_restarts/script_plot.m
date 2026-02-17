6f1d5428bf105c542973d55dde5803e4c16203b5
Bennet <115168210+b-gebken@users.noreply.github.com>
2026-02-17 13:48:25 +0100

    Update script_plot.m

--- Example_restarts/script_plot.m
+++ Example_restarts/script_plot.m
@@ -39,7 +39,7 @@ figure;
 plot(0:size(x_arr,2)-1,log10(result_m_minus_1.f_arr - f_min),'r.-','MarkerSize',ms,'LineWidth',lw);
 hold on
 plot(0:size(x_arr,2)-1,log10(f_arr - f_min),'k.-','MarkerSize',ms,'LineWidth',lw);
-xline(reset_inds,'k--','LineWidth',1.25)
+xline(reset_inds-1,'k--','LineWidth',1.25)
 
 h1 = plot(-100,-100,'k.-','MarkerSize',13,'LineWidth',1.15); % Dummy plot for legend
 h2 = plot(-100,-100,'r.-','MarkerSize',13,'LineWidth',1.15); % Dummy plot for legend
@@ -49,8 +49,9 @@ legend([h1,h2],{'$m$','$m-1$'},...
 xlim([0,1440])
 ylim([-12.5,8])
 xlabel('$k$','Interpreter','latex')
-xticks(0:250:1500)
+xticks(0:320:1500)
 axis square
+grid on
 
 set(gca,'linewidth',1.1)
 set(gca,'fontsize',15)
@@ -70,8 +71,8 @@ yticklabels(new_ticks_cell)
     
     ax = axes('position',[.45 .515 .4 .4]);
     
-    min_ind = 801-20;
-    max_ind = 961+20;
+    min_ind = 881-10;
+    max_ind = 961+10;
     zoom_inds = min_ind:max_ind;
     
     plot(zoom_inds-1,log10(f_arr(zoom_inds) - f_min),'k.-','MarkerSize',ms,'LineWidth',1);
@@ -79,11 +80,12 @@ yticklabels(new_ticks_cell)
     xline(reset_inds(reset_inds >= min_ind & reset_inds <= max_ind)-1,'k--','LineWidth',1.25);
     
     xlim([min_ind,max_ind])
-    ylim([-8.2,-6.3])
+    ylim([-8.2,-6.9])
     axis square
+    grid on
     ax.ClippingStyle = "rectangle";
     
-    xticks(800:100:1000)
+    xticks(800:80:1000)
     yticks([-9,-8,-7])
     
     % Log tick labeling (y)
@@ -146,20 +148,21 @@ figure;
 
 h1 = plot(0:size(x_arr,2)-2,log10(t_arr),'k.','MarkerSize',11,'LineWidth',lw);
 hold on
-xline(reset_inds,'k--','LineWidth',1.25)
+xline(reset_inds-1,'k--','LineWidth',1.25)
 
 legend(h1,{'$t_k$'},...
-    'Interpreter','latex','FontSize',15,'Location','southwest');
+    'Interpreter','latex','FontSize',15,'Position',[0.6873 0.1683 0.0803 0.0667]);
 
 set(gca,'linewidth',1.1)
 set(gca,'fontsize',15)
 
 xlim([0,1441])
-ylim([-18,0])
+ylim([-18.25,-1.25])
 xlabel('$k$','Interpreter','latex');
-xticks(0:250:1500)
+xticks(0:320:1500)
 
 axis square
+grid on
 
 set(gca,'linewidth',1.1)
 set(gca,'fontsize',15)
@@ -171,6 +174,41 @@ for i = 1:numel(old_ticks)
     new_ticks_cell{i} = ['10^{',num2str(old_ticks(i)),'}'];
 end
 yticklabels(new_ticks_cell)
+
+% Subfigure -----------------------------------------------------------
+    
+    bk_ax = axes('position',[.23 .15 .272 .320]);
+    set(gca,'XColor','none','YColor','none')
+
+    ax = axes('position',[.2 .2 .4 .27]);
+
+    min_ind = 1280-5;
+    max_ind = 1280+5;
+    zoom_inds = min_ind:max_ind;
+
+    plot(zoom_inds-1,log10(t_arr(zoom_inds)),'k.','MarkerSize',11,'LineWidth',lw);
+    hold on
+    xline(reset_inds(reset_inds >= min_ind & reset_inds <= max_ind)-1,'k--','LineWidth',1.25);
+
+    xlim([min_ind-1-0.5,max_ind-1+0.5])
+    ylim([-15,-1.0])
+    axis square
+    grid on
+    ax.ClippingStyle = "rectangle";
+
+    xticks(1280)
+    % yticks([-9,-8,-7])
+
+    % Log tick labeling (y)
+    old_ticks = yticks;
+    new_ticks_cell = cell(numel(old_ticks),1);
+    for i = 1:numel(old_ticks)
+        new_ticks_cell{i} = ['10^{',num2str(old_ticks(i)),'}'];
+    end
+    yticklabels(new_ticks_cell)
+
+    set(gca,'linewidth',1.1)
+    set(gca,'fontsize',10)
 
 if(save_flag)
     export_fig 'plot3' '-png' '-r500' '-transparent'
